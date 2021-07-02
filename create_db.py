2f2f8b425505db9557488b055cc9a3a36b396ac7
Sebastian Luarte Becar <sluarteb@gmail.com>
2021-07-02 15:20:06 -0700

    02/07

--- create_db.py
+++ create_db.py
@@ -5,47 +5,384 @@ MONGO_URI = 'mongodb://localhost'
 client = MongoClient(MONGO_URI)
 
 db = client['Tarea']
-collection = db['Usuarios']
-x = True
+collection = db['Audios']
 
 
-collection.insert_one({ 
-	"RUT": Rut,
-	"nombre": nombre,
-	"apellido": apellido,
-	"Archivo_Audio" : [{ 
-			"ID_archivo" : None,
-			"formato" : None,
-			"exterior" : None,
-			"fecha_grabacion" : None,
-			"duracion" : None,
-			"longitud" : None,
-			"latitud" : None,
-			"ciudad" : None,
-			"fecha upload" : None,
-			"RUT_Usuario" : None,
-			"Segmentos" : [{
-				"ID_segmento" : None,
-				"formato" : None,
-				"duracion" : None,
-				"inicio" : None,
-				"fin" : None,
-				"ID_archivo" : None
+def AgegarDato(collection):
+	collection.insert_many([
+	{ 
+		"ID_archivo" : 1,
+		"sonido": None,
+		"formato" : "wav",
+		"exterior" : "interior",
+		"fecha_grabacion" : "30-06-2021",
+		"duracion" : 60,
+		"latitud" : -39.82298137383703,
+		"longitud" : -73.24279073177289,
+		"ciudad" : "Valdivia",
+		"fecha upload" : "30-06-2021",
+		"Usuario" : {
+			"RUT": "22507458-5",
+			"nombre": "Cesar",
+			"apellido": "Gracia",
+		},
+		"Segmentos" : [{
+			"ID_segmento" : 100,
+			"formato" : "wav",
+			"duracion" : 60,
+			"inicio" : 0,
+			"fin" : 60,
+			"Analizar": [{
+				"tipo_analizador" : "Humano",
+				"porcentaje" : None,
+				"nombre_fuente": "Impresora",
+				"descripción": "Impresora laser",
+				"nombre_fuente_categoria": "Mecanico"		
+			},
+			{
+				"tipo_analizador" : "Maquina",
+				"porcentaje" : 90,
+				"nombre_fuente": "Impresora",
+				"descripción": "Impresora laser.",
+				"nombre_fuente_categoria": "Mecanico"			
 			}]
 		}]
-})
 
-collection = db['Fuentes']
-collection.insert_one({ 
-	"nombre_fuente": None,
-	"descripción": None,
-	"nombre_super_categoria": None
-})
+	},
+	
+	{ 
+		"ID_archivo" : 2,
+		"sonido": None,
+		"formato" : "wav",
+		"exterior" : "exterior",
+		"fecha_grabacion" : "25-06-2021",
+		"duracion" : 63,
+		"latitud" : -39.82768169317065,
+		"longitud" : -73.24020215419479,
+		"ciudad" : "Valdivia",
+		"fecha upload" : "25-06-2021",
+		"Usuario" : {
+			"RUT": "15606233-2",
+			"nombre": "Magdalena",
+			"apellido": "Cobo",
+		},
+		"Segmentos" : [{
+			"ID_segmento" : 101,
+			"formato" : "wav",
+			"duracion" : 63,
+			"inicio" : 0,
+			"fin" : 63,
+			"Analizar": [{
+				"tipo_analizador" : "Maquina",
+				"porcentaje" : 95,
+				"nombre_fuente": "Auto",
+				"descripción": "Auto encendiendo y arrancando.",
+				"nombre_fuente_categoria": "Vehiculos"		
+			}]
+		}]
 
-collection = db['Analizar']
-collection.insert_one({ 
-	"nombre" : None,
-	"ID_segmento" : None,
-	"nombre_fuente" : None,
-	"porcentaje" : None
-})
+	},
+	
+	{ 
+		"ID_archivo" : 3,
+		"sonido": None,
+		"formato" : "wav",
+		"exterior" : "exterior",
+		"fecha_grabacion" : "28-06-2021",
+		"duracion" : 66,
+		"latitud" : -39.809651803554864,
+		"longitud" : -73.25676351029286,
+		"ciudad" : "Valdivia",
+		"fecha upload" : "28-06-2021",
+		"Usuario" : {
+			"RUT": "20056044-2",
+			"nombre": "Sagrario",
+			"apellido": "Benavente",
+		},
+		"Segmentos" : [{
+			"ID_segmento" : 102,
+			"formato" : "wav",
+			"duracion" : 66,
+			"inicio" : 0,
+			"fin" : 66,
+			"Analizar": [{
+				"tipo_analizador" : "Humano",
+				"porcentaje" : None,
+				"nombre_fuente": "Lluvia",
+				"descripción": "Sonido de lluvia",
+				"nombre_fuente_categoria": "Climáticos y Medio ambientales"		
+			}]
+		}]
+
+	},
+
+	{ 
+		"ID_archivo" : 4,
+		"sonido": None,
+		"formato" : "wav",
+		"exterior" : "exterior",
+		"fecha_grabacion" : "27-06-2021",
+		"duracion" : 29,
+		"latitud" : -39.81877264264198,
+		"longitud" : -73.24969819123932,
+		"ciudad" : "Valdivia", 
+		"fecha upload" : "27-06-2021",
+		"Usuario" : {
+			"RUT": "16751646-7",
+			"nombre": "Karina",
+			"apellido": "Mercado",
+		},
+		"Segmentos" : [{
+			"ID_segmento" : 103,
+			"formato" : "wav",
+			"duracion" : 29,
+			"inicio" : 0,
+			"fin" : 29,
+			"Analizar": [{
+				"tipo_analizador" : "Maquina",
+				"porcentaje" : 94,
+				"nombre_fuente": "Viento",
+				"descripción": "Sonido de viento en una tormenta.",
+				"nombre_fuente_categoria": "Climáticos y Medio ambientales"			
+			}]
+		}]
+
+	},
+	
+	{ 
+		"ID_archivo" : 5,
+		"sonido": None,
+		"formato" : "wav",
+		"exterior" : "exterior",
+		"fecha_grabacion" : "26-06-2021",
+		"duracion" : 30,
+		"latitud" : -39.82443658089787,
+		"longitud" : -73.2271502531366,
+		"ciudad" : "Valdivia", 
+		"fecha upload" : "26-06-2021",
+		"Usuario" : {
+			"RUT": "14337124-7",
+			"nombre": "Mauricio",
+			"apellido": "Rovira",
+		},
+		"Segmentos" : [{
+			"ID_segmento" : 104,
+			"formato" : "wav",
+			"duracion" : 30,
+			"inicio" : 0,
+			"fin" : 30,
+			"Analizar": [{
+				"tipo_analizador" : "Maquina",
+				"porcentaje" : 89,
+				"nombre_fuente": "Sirena",
+				"descripción": "Sonido sirena de bomberos.",
+				"nombre_fuente_categoria": "Alertas"			
+			}]
+		}]
+
+	},
+	
+	{ 
+		"ID_archivo" : 6,
+		"sonido": None,
+		"formato" : "wav",
+		"exterior" : "exterior",
+		"fecha_grabacion" : "30-06-2021",
+		"duracion" : 46,
+		"latitud" : -39.80492182672446,
+		"longitud" : -73.25041036829789,
+		"ciudad" : "Valdivia", 
+		"fecha upload" : "30-06-2021",
+		"Usuario" : {
+			"RUT": "13959169-0",
+			"nombre": "Alma",
+			"apellido": "Lloret",
+		},
+		"Segmentos" : [{
+			"ID_segmento" : 105,
+			"formato" : "wav",
+			"duracion" : 46,
+			"inicio" : 0,
+			"fin" : 46,
+			"Analizar": [{
+				"tipo_analizador" : "Humano",
+				"porcentaje" : None,
+				"nombre_fuente": "Motosierra",
+				"descripción": "Sonido de motosierra cortando madera.",
+				"nombre_fuente_categoria": "Mecanico"			
+			}]
+		}]
+
+	},
+	
+	{ 
+		"ID_archivo" : 7,
+		"sonido": None,
+		"formato" : "wav",
+		"exterior" : "interior",
+		"fecha_grabacion" : "28-06-2021",
+		"duracion" : 21,
+		"latitud" : -39.83313732002285,
+		"longitud" : -73.23865156445318,
+		"ciudad" : "Valdivia", 
+		"fecha upload" : "28-06-2021",
+		"Usuario" : {
+			"RUT": "18122610-2",
+			"nombre": "Lluc",
+			"apellido": "Simon",
+		},
+		"Segmentos" : [{
+			"ID_segmento" : 106,
+			"formato" : "wav",
+			"duracion" : 21,
+			"inicio" : 0,
+			"fin" : 21,
+			"Analizar": [{
+				"tipo_analizador" : "Humano",
+				"porcentaje" : None,
+				"nombre_fuente": "Perro",
+				"descripción": "Perro ladrando al interior de una casa.",
+				"nombre_fuente_categoria": "Animales"			
+			}]
+		}]
+
+	},
+	
+	{ 
+		"ID_archivo" : 7,
+		"sonido": None,
+		"formato" : "wav",
+		"exterior" : "interior",
+		"fecha_grabacion" : "28-06-2021",
+		"duracion" : 22,
+		"latitud" : -39.83313732002285,
+		"longitud" : -73.23865156445318,
+		"ciudad" : "Valdivia", 
+		"fecha upload" : "28-06-2021",
+		"Usuario" : {
+			"RUT": "18122610-2",
+			"nombre": "Lluc",
+			"apellido": "Simon",
+		},
+		"Segmentos" : [{
+			"ID_segmento" : 106,
+			"formato" : "wav",
+			"duracion" : 22,
+			"inicio" : 0,
+			"fin" : 22,
+			"Analizar": [{
+				"tipo_analizador" : "Humano",
+				"porcentaje" : None,
+				"nombre_fuente": "Perro",
+				"descripción": "Perro ladrando al interior de una casa.",
+				"nombre_fuente_categoria": "Animales"			
+			}]
+		}]
+
+	},
+	
+	{ 
+		"ID_archivo" : 8,
+		"sonido": None,
+		"formato" : "wav",
+		"exterior" : "exterior",
+		"fecha_grabacion" : "27-06-2021",
+		"duracion" : 49,
+		"latitud" : -39.80196201802745,
+		"longitud" : -73.26103097922493,
+		"ciudad" : "Valdivia", 
+		"fecha upload" : "27-06-2021",
+		"Usuario" : {
+			"RUT": "16958401-k",
+			"nombre": "Graciela",
+			"apellido": "Dorado",
+		},
+		"Segmentos" : [{
+			"ID_segmento" : 107,
+			"formato" : "wav",
+			"duracion" : 49,
+			"inicio" : 0,
+			"fin" : 49,
+			"Analizar": [{
+				"tipo_analizador" : "Maquina",
+				"porcentaje" : 97,
+				"nombre_fuente": "Pajaro",
+				"descripción": "Sonido de pajaro en el bosque.",
+				"nombre_fuente_categoria": "Animales"			
+			}]
+		}]
+
+	},
+	
+	{ 
+		"ID_archivo" : 9,
+		"sonido": None,
+		"formato" : "wav",
+		"exterior" : "interior",
+		"fecha_grabacion" : "25-06-2021",
+		"duracion" : 76,
+		"latitud" : -39.815068229211036,
+		"longitud" : -73.24639439075622,
+		"ciudad" : "Valdivia", 
+		"fecha upload" : "25-06-2021",
+		"Usuario" : {
+			"RUT": "12450795-2",
+			"nombre": "Candida",
+			"apellido": "Alcazar",
+		},
+		"Segmentos" : [{
+			"ID_segmento" : 108,
+			"formato" : "wav",
+			"duracion" : 76,
+			"inicio" : 0,
+			"fin" : 76,
+			"Analizar": [{
+				"tipo_analizador" : "Maquina",
+				"porcentaje" : 85,
+				"nombre_fuente": "Voces",
+				"descripción": "Sonido de voces humanas en un restaurant.",
+				"nombre_fuente_categoria": "Humanos"			
+			}]
+		}]
+
+	},
+	
+	{ 
+		"ID_archivo" : 10,
+		"sonido": None,
+		"formato" : "wav",
+		"exterior" : "interior",
+		"fecha_grabacion" : "27-06-2021",
+		"duracion" : 25,
+		"latitud" : -39.83628621780177,
+		"longitud" : -73.21570963049898,
+		"ciudad" : "Valdivia", 
+		"fecha upload" : "27-06-2021",
+		"Usuario" : {
+			"RUT": "13276536-7",
+			"nombre": "Jose",
+			"apellido": "Tellez",
+		},
+		"Segmentos" : [{
+			"ID_segmento" : 109,
+			"formato" : "wav",
+			"duracion" : 25,
+			"inicio" : 0,
+			"fin" : 25,
+			"Analizar": [{
+				"tipo_analizador" : "Maquina",
+				"porcentaje" : 97,
+				"nombre_fuente": "Gato",
+				"descripción": "Sonido de ronroneo y maullido de gato.",
+				"nombre_fuente_categoria": "Animales"			
+			}]
+		}]
+
+	}
+	
+	
+	])
+
+
+AgegarDato(collection)
+
---
+++ mapa.py
@@ -0,0 +1,199 @@
+import folium
+import branca
+import webbrowser
+from pymongo import MongoClient
+
+from playsound import playsound
+
+mapa = folium.Map(location = [-39.81730435463257, -73.24257650930436], zoom_start=13 ,control_scale = True)
+
+
+def MostrarDia(mapa):
+	MONGO_URI = 'mongodb://localhost'
+
+	client = MongoClient(MONGO_URI)
+
+	db = client['Tarea']
+	collection = db['Audios']
+	
+
+	for documento in collection.find():
+		html = "<p>ID: " + str(documento['ID_archivo']) + " </p>"
+		html += "<p>Latitud: " + str(documento['latitud']) + " </p>"
+		html += "<p>Longitud: " + str(documento['longitud']) + "</p>"
+		html += "<p>Duracion : " + str(documento['duracion']) + "s</p>"
+		html += "<p>Formato: " + str(documento['formato']) + "</p>"
+		html += "<p>Exterior: " + str(documento['exterior']) + "</p>"
+		html += "<p>Fecha de grabacion: " + str(documento['fecha_grabacion']) + "</p>"
+		html += "<p>Nombre de la fuente: " + str(documento['Segmentos'][0]['Analizar'][0]['nombre_fuente']) + "</p>"
+		html += "<p>Categoria: " + str(documento['Segmentos'][0]['Analizar'][0]['nombre_fuente_categoria']) + "</p>"
+		html += '''
+			<audio controls>
+			    <source src="file:///home/mcsebe/Desktop/UACH/Datos/INFO133_2021_Tarea1/impresora1.wav" type="audio/wav">
+				Tu navegador no soporta audio HTML5.
+			</audio>
+			'''
+			
+		iframe3 = branca.element.IFrame(html=html, width=800, height=340)
+	
+		marcador1 = folium.Marker(location= [documento['latitud'], documento['longitud']],
+			popup = folium.Popup(iframe3, max_width=340),
+			icon = folium.Icon(color="black")
+		)
+		marcador1.add_to(mapa)
+		
+MostrarDia(mapa)
+
+mapa.save("mapa_test.html")
+webbrowser.open("mapa_test.html")
+
+
+#############
+
+
+from tkinter import *
+from tkinter import messagebox as MessageBox
+
+def send_data():
+	mapa = folium.Map(location = [-39.81730435463257, -73.24257650930436], zoom_start=13 ,control_scale = True)
+
+	MONGO_URI = 'mongodb://localhost'
+
+	client = MongoClient(MONGO_URI)
+
+	db = client['Tarea']
+	collection = db['Audios']
+	
+	fechavalor_info = fechavalor.get()
+	Humanos_info = bool(Humanos.get())
+	Musica_info = bool(Musica.get())
+	Animales_info = bool(Animales.get())
+	Ambientales_info = bool(Ambientales.get())
+	Mecanicos_info = bool(Mecanicos.get())
+	Vehiculos_info = bool(Vehiculos.get())
+	Alertas_info = bool(Alertas.get())
+    
+	if (fechavalor_info != "" and fechavalor_info != "-"):
+		for documento in collection.find({ "fecha_grabacion": fechavalor_info}):
+
+			cond1 = (Humanos_info and str(documento['Segmentos'][0]['Analizar'][0]['nombre_fuente_categoria']) == "Humanos")
+			cond2 = (Musica_info and str(documento['Segmentos'][0]['Analizar'][0]['nombre_fuente_categoria']) == "Musica")
+			cond3 = (Animales_info and str(documento['Segmentos'][0]['Analizar'][0]['nombre_fuente_categoria']) == "Animales")
+			cond4 = (Ambientales_info and str(documento['Segmentos'][0]['Analizar'][0]['nombre_fuente_categoria']) == "Climáticos y Medio ambientales")
+			cond5 = (Mecanicos_info and (str(documento['Segmentos'][0]['Analizar'][0]['nombre_fuente_categoria']) == "Mecanico"))
+			cond6 = (Vehiculos_info and str(documento['Segmentos'][0]['Analizar'][0]['nombre_fuente_categoria']) == "Vehiculos")
+			cond7 = (Alertas_info and str(documento['Segmentos'][0]['Analizar'][0]['nombre_fuente_categoria']) == "Alertas")
+			
+			if (cond1 or cond2 or cond3 or cond4 or cond5 or cond6 or cond7):
+				html = "<p>ID: " + str(documento['ID_archivo']) + " </p>"
+				html += "<p>Latitud: " + str(documento['latitud']) + " </p>"
+				html += "<p>Longitud: " + str(documento['longitud']) + "</p>"
+				html += "<p>Duracion : " + str(documento['duracion']) + "s</p>"
+				html += "<p>Formato: " + str(documento['formato']) + "</p>"
+				html += "<p>Exterior: " + str(documento['exterior']) + "</p>"
+				html += "<p>Fecha de grabacion: " + str(documento['fecha_grabacion']) + "</p>"
+				html += "<p>Nombre de la fuente: " + str(documento['Segmentos'][0]['Analizar'][0]['nombre_fuente']) + "</p>"
+				html += "<p>Categoria: " + str(documento['Segmentos'][0]['Analizar'][0]['nombre_fuente_categoria']) + "</p>"
+
+				iframe3 = branca.element.IFrame(html=html, width=800, height=340)
+			
+				marcador1 = folium.Marker(location= [documento['latitud'], documento['longitud']],
+					popup = folium.Popup(iframe3, max_width=340),
+					icon = folium.Icon(color="black")
+				)
+				marcador1.add_to(mapa)
+	else:
+		for documento in collection.find():
+
+			cond1 = (Humanos_info and str(documento['Segmentos'][0]['Analizar'][0]['nombre_fuente_categoria']) == "Humanos")
+			cond2 = (Musica_info and str(documento['Segmentos'][0]['Analizar'][0]['nombre_fuente_categoria']) == "Musica")
+			cond3 = (Animales_info and str(documento['Segmentos'][0]['Analizar'][0]['nombre_fuente_categoria']) == "Animales")
+			cond4 = (Ambientales_info and str(documento['Segmentos'][0]['Analizar'][0]['nombre_fuente_categoria']) == "Climáticos y Medio ambientales")
+			cond5 = (Mecanicos_info and (str(documento['Segmentos'][0]['Analizar'][0]['nombre_fuente_categoria']) == "Mecanico"))
+			cond6 = (Vehiculos_info and str(documento['Segmentos'][0]['Analizar'][0]['nombre_fuente_categoria']) == "Vehiculos")
+			cond7 = (Alertas_info and str(documento['Segmentos'][0]['Analizar'][0]['nombre_fuente_categoria']) == "Alertas")
+			
+
+			if (cond1 or cond2 or cond3 or cond4 or cond5 or cond6 or cond7):
+
+				html = "<p>ID: " + str(documento['ID_archivo']) + " </p>"
+				html += "<p>Latitud: " + str(documento['latitud']) + " </p>"
+				html += "<p>Longitud: " + str(documento['longitud']) + "</p>"
+				html += "<p>Duracion : " + str(documento['duracion']) + "s</p>"
+				html += "<p>Formato: " + str(documento['formato']) + "</p>"
+				html += "<p>Exterior: " + str(documento['exterior']) + "</p>"
+				html += "<p>Fecha de grabacion: " + str(documento['fecha_grabacion']) + "</p>"
+				html += "<p>Nombre de la fuente: " + str(documento['Segmentos'][0]['Analizar'][0]['nombre_fuente']) + "</p>"
+				html += "<p>Categoria: " + str(documento['Segmentos'][0]['Analizar'][0]['nombre_fuente_categoria']) + "</p>"
+
+				iframe3 = branca.element.IFrame(html=html, width=800, height=300)
+			
+				marcador1 = folium.Marker(location= [documento['latitud'], documento['longitud']],
+					popup = folium.Popup(iframe3, max_width=300),
+					icon = folium.Icon(color="black")
+				)
+				marcador1.add_to(mapa)
+				
+	def play():
+	    playsound('impresora1.wav')
+	 
+	 
+	play_button = Button(mywindow, text="Play Song", font=("Helvetica", 12), command=play)
+	play_button.place(x=250, y=205)
+    
+	mapa.save("mapa_test.html")
+	webbrowser.open("mapa_test.html")
+
+
+
+# Create new instance - Class Tk()
+mywindow = Tk()
+mywindow.geometry("450x400")
+mywindow.title("Mapa de Audios en Valdivia")
+mywindow.resizable(False, False)
+mywindow.config(background="#F8F7EF")
+main_title = Label(text="Bienvenido \nLlene los datos que desea filtrar y aprete siguiente", font=(
+    "Arial", 14), bg="#A2BAB9", fg="black", width="500", height="2")
+main_title.pack()
+
+opciones1 = ["-","25-06-2021","26-06-2021","27-06-2021","28-06-2021","29-06-2021","30-06-2021"]
+
+# Define Label Fields
+fecha_label = Label(
+    text="Seleccione la Fecha: ", bg="#F8F7EF", font=("Arial", 12))
+fecha_label.place(x=150, y=70)
+
+
+estadoFinal_label = Label(
+    text="Seleccione la categoria: ", bg="#F8F7EF", font=("Arial", 12))
+estadoFinal_label.place(x=150, y=150)
+
+
+fechavalor = StringVar()
+fecha = OptionMenu(mywindow, fechavalor,*opciones1)
+fecha.config(width=10)
+fecha.place(x=150, y=105)
+
+Humanos = IntVar()
+Checkbutton(mywindow, text="Humanos", variable=Humanos, bg="#F8F7EF").place(x=50, y=180)
+Musica = IntVar()
+Checkbutton(mywindow, text="Música", variable=Musica, bg="#F8F7EF").place(x=50, y=210)
+Animales = IntVar()
+Checkbutton(mywindow, text="Animales", variable=Animales, bg="#F8F7EF").place(x=50, y=240)
+Ambientales = IntVar()
+Checkbutton(mywindow, text="Climáticos y Medio ambientales", variable=Ambientales, bg="#F8F7EF").place(x=50, y=270)
+Mecanicos = IntVar()
+Checkbutton(mywindow, text="Mecanicos", variable=Mecanicos, bg="#F8F7EF").place(x=300, y=180)
+Vehiculos = IntVar()
+Checkbutton(mywindow, text="Vehiculos", variable=Vehiculos, bg="#F8F7EF").place(x=300, y=210)
+Alertas = IntVar()
+Checkbutton(mywindow, text="Alertas", variable=Alertas, bg="#F8F7EF").place(x=300, y=240)
+
+
+
+# Submit Button
+siguiente_btn = Button(mywindow, text="Siguiente", width="30",
+                       height="2", command=send_data, bg="#A2BAB9")
+siguiente_btn.place(x=125, y=300)
+
+mywindow.mainloop()
